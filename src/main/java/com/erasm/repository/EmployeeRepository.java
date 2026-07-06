1bb82feb3935d0cc1b55808676f43872f25acfa6
PreranaJagadish <preranasantoshi@gmail.com>
2026-07-06 19:09:08 +0530

    feat: initial ERASM project

--- src/main/java/com/erasm/repository/EmployeeRepository.java
+++ src/main/java/com/erasm/repository/EmployeeRepository.java
@@ -1,49 +1,13 @@
 package com.erasm.repository;
 
 import com.erasm.entity.Employee;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
-    @EntityGraph(attributePaths = {
-            "user",
-            "role",
-            "employeeSkills",
-            "employeeSkills.skill",
-            "certifications"
-    })
-    @Override
-    Optional<Employee> findById(Long id);
-
-    @EntityGraph(attributePaths = {
-            "user",
-            "role",
-            "employeeSkills",
-            "employeeSkills.skill",
-            "certifications"
-    })
-    @Override
-    List<Employee> findAll();
-
-    @EntityGraph(attributePaths = {
-            "user",
-            "role",
-            "employeeSkills",
-            "employeeSkills.skill",
-            "certifications"
-    })
     Optional<Employee> findByUser_Id(Long userId);
 
-    @EntityGraph(attributePaths = {
-            "user",
-            "role",
-            "employeeSkills",
-            "employeeSkills.skill",
-            "certifications"
-    })
     Optional<Employee> findByUser_Email(String email);
 }
